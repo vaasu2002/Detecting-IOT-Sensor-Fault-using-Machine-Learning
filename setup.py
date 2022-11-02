40d7851bd8f05f88fa66070854e9f7d851fabbb7
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2022-11-02 19:25:56 +0530

    basic structure update 01

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Sensor",
+    version="0.0.0",
+    author="vaasu",
+    author_email="bishtvaasu@gmail.com",
+    packages=find_packages()
+)

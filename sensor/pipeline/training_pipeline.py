3dff100125b15fe5093ee81556160224bd178cca
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2022-11-06 14:31:17 +0530

    data validation update

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -2,9 +2,12 @@ import sys
 import os
 from sensor.exception import SensorException
 from sensor.logger import logging
-from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
+
+
 class TrainPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
@@ -21,18 +24,43 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e,sys)
     
-    def stop_data_validation(self):
-        pass
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation_config= DataValidationConfig(training_pipeline_config = self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+                                             data_validation_config=data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+        except Exception as e:
+            raise SensorException(e,sys) 
+
     def start_data_transformation(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys) 
+    
     def start_data_trainer(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys) 
+    
     def start_data_evaluation(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys) 
+    
     def start_data_pusher(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e,sys) 
+    
     def run_pipeline(self):
         try:
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as e:
             raise SensorException(e,sys)        

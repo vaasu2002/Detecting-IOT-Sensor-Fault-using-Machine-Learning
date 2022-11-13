1d45d6ea7156ba2a9eadc8642afbcfd0b3934065
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2022-11-13 13:06:38 +0530

    DataTransformationConfig

--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -2,12 +2,14 @@ from datetime import datetime
 import os
 from sensor.constant import training_pipeline
 
+
 class TrainingPipelineConfig:
     def __init__(self,timestamp=datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name: str = training_pipeline.PIPELINE_NAME
         self.artifact_dir: str = os.path.join(training_pipeline.ARTIFACT_DIR,timestamp)
         self.timestamp: str = timestamp
+
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
@@ -59,3 +61,16 @@ class DataValidationConfig:
                 training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
                 training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
             )
+
+class DataTransformationConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+            
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCSSING_OBJECT_FILE_NAME,)

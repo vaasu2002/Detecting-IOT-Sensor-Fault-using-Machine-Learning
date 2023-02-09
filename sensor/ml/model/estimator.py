9cdf70af8e291264c11b390159d076c270b40e3e
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-02-09 15:35:24 +0530

    updated estimator

--- sensor/ml/model/estimator.py
+++ sensor/ml/model/estimator.py
@@ -1,6 +1,6 @@
 # importing dependencies
 
-#from sensor.constant.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
+from sensor.constant.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
 import os
 
 
@@ -40,6 +40,39 @@ class SensorModel:
         except Exception as e:
             raise e
 
+class ModelResolver:
 
-  
+    def __init__(self,model_dir=SAVED_MODEL_DIR):
+        try:
+            self.model_dir = model_dir
+
+        except Exception as e:
+            raise e
+
+    def get_best_model_path(self,)->str:
+        try:
+            timestamps = list(map(int,os.listdir(self.model_dir)))
+            latest_timestamp = max(timestamps)
+            latest_model_path= os.path.join(self.model_dir,f"{latest_timestamp}",MODEL_FILE_NAME)
+            return latest_model_path
+        except Exception as e:
+            raise e
+
+    def is_model_exists(self)->bool:
+        try:
+            if not os.path.exists(self.model_dir):
+                return False
+
+            timestamps = os.listdir(self.model_dir)
+            if len(timestamps)==0:
+                return False
+            
+            latest_model_path = self.get_best_model_path()
+
+            if not os.path.exists(latest_model_path):
+                return False
+
+            return True
+        except Exception as e:
+            raise e
 
